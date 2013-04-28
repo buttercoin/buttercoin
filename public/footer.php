b03a724182536655463fe6bc791e2a9945fd23dc
Martin Harding <martin@sutvar.com>
2013-04-27 23:20:25 -0400

    Delete signup.html and add header/footer php

---
+++ public/footer.php
@@ -0,0 +1,10 @@
+		<footer>
+			<p>© Buttercoin 2013</p>
+		</footer>
+	</div> <!-- /container -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+	<script src="/js/vendor/bootstrap.min.js"></script>
+	<script src="/js/main.js"></script>
+</body>
+</html>
