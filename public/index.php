2ed4e828860dad4e1329313b983298373e3ae4c2
JulianNorton <julian@juliannorton.com>
2013-04-27 23:16:43 -0400

    php index, signup tweaks

---
+++ public/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>Buttercoin</title>
+	<link href="less/bootstrap.css" rel="stylesheet">
+	<link href="less/responsive.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<!-- This code is taken from http://twitter.github.com/bootstrap/examples/hero.html   -->
+
+	<div class="navbar navbar-inverse">
+		<div class="navbar-inner">
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</a>
+				<a class="brand" href="#">Buttercoin</a>
+				<div class="nav-collapse collapse">
+					<ul class="nav">
+						<li class="active"><a href="/">Home</a></li>
+						<li><a target="_blank" href="https://github.com/buttercoin/buttercoin/blob/master/README.md">About</a></li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Get Involved <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a target="_blank" href="https://github.com/buttercoin/buttercoin">Github</a></li>
+								<li><a target="_blank" href="https://buttercoin.hackpad.com">Hackpad</a></li>
+								<li><a href="http://www.reddit.com/r/Buttercoin">Reddit</a></li>
+							</ul>
+						</li>
+					</ul>
+					<!-- <form class="navbar-form pull-right">
+						<input class="span2" type="text" placeholder="Email">
+						<input class="span2" type="password" placeholder="Password">
+						<button type="submit" class="btn">Sign in</button>
+					</form> -->
+				</div><!--/.nav-collapse -->
+			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="hero-unit">
+        <h1>Buttercoin</h1>
+        <p class="lead">an open-source exchange…</p>
+        <p><a href="signup.html" class="btn btn-primary btn-large">Sign up »</a></p>
+        <!-- <div class="input-prepend inline-block-level">
+        	<div class="">
+        		<span class="add-on"><i class="icon-envelope"></i></span>
+        		<input class="focused span11" type="text" id="inputEmail" placeholder="Email">
+        	</div>
+        </div> -->
+      </div>
+		<footer>
+			<p>© Buttercoin 2013</p>
+		</footer>
+	</div> <!-- /container -->
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+	<script src="/js/vendor/bootstrap.min.js"></script>
+	<script src="/js/main.js"></script>
+</body>
+</html>
